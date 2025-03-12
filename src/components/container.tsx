7fab123a4cd014693d20f80bdbe2271c1e2f995f
pphatdev <leatsophat02@gmail.com>
2025-03-12 18:08:54 +0700

    Updated Dynamic header display

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -7,43 +7,49 @@ import { SidebarInset, SidebarProvider, SidebarTrigger } from "./ui/sidebar"
 import { Breadcrumbs } from "@/types/breadcrumbs"
 import { Tiles } from "./ui/tiles";
 
-export const Container = ({ children, breadcrumb = [] }: { children: React.ReactNode, breadcrumb: Breadcrumbs[] }) => {
+export const Container = ({ children, breadcrumb = [], showHeader = true }: { children: React.ReactNode, breadcrumb: Breadcrumbs[], showHeader?: Boolean }) => {
     return (
         <SidebarProvider>
             <AppSidebar />
             <SidebarInset className="relative">
                 <NextTopLoader />
-                <section className="fixed w-full h-72 top-0 z-0 overflow-hidden rounded-xl">
-                    <Tiles
-                        className="max-h-72 mx-auto opacity-30"
-                        rows={100}
-                        cols={30}
-                        tileSize="sm"
-                    />
-                    <div className="h-72 top-0 fixed w-full bg-gradient-to-t from-background to-transparent"></div>
-                </section>
-                <header className="flex z-50 h-16 shrink-0 items-center gap-2">
-                    <div className="flex items-center gap-2 px-4">
-                        <SidebarTrigger className="-ml-1" />
-                        <Separator orientation="vertical" className="mr-2 h-4" />
-                        <Breadcrumb>
-                            <BreadcrumbList className="flex-row flex-nowrap">
-                                {breadcrumb.map((item, index) => (
-                                    <React.Fragment key={index}>
-                                        <BreadcrumbItem className="line-clamp-1">
-                                            {item.href ? (
-                                                <BreadcrumbLink href={item.href}>{item.label}</BreadcrumbLink>
-                                            ) : (
-                                                <BreadcrumbPage>{item.label}</BreadcrumbPage>
-                                            )}
-                                        </BreadcrumbItem>
-                                        {index < breadcrumb.length - 1 && <BreadcrumbSeparator />}
-                                    </React.Fragment>
-                                ))}
-                            </BreadcrumbList>
-                        </Breadcrumb>
-                    </div>
-                </header>
+                {
+                    showHeader && (
+                        <>
+                            <section className="fixed w-full h-72 top-0 z-0 overflow-hidden rounded-xl">
+                                <Tiles
+                                    className="max-h-72 mx-auto opacity-30"
+                                    rows={100}
+                                    cols={30}
+                                    tileSize="sm"
+                                />
+                                <div className="h-72 top-0 fixed w-full bg-gradient-to-t from-background to-transparent"></div>
+                            </section>
+                            <header className="flex z-50 h-16 shrink-0 items-center gap-2">
+                                <div className="flex items-center gap-2 px-4">
+                                    <SidebarTrigger className="-ml-1" />
+                                    <Separator orientation="vertical" className="mr-2 h-4" />
+                                    <Breadcrumb>
+                                        <BreadcrumbList className="flex-row flex-nowrap">
+                                            {breadcrumb.map((item, index) => (
+                                                <React.Fragment key={index}>
+                                                    <BreadcrumbItem className="line-clamp-1">
+                                                        {item.href ? (
+                                                            <BreadcrumbLink href={item.href}>{item.label}</BreadcrumbLink>
+                                                        ) : (
+                                                            <BreadcrumbPage>{item.label}</BreadcrumbPage>
+                                                        )}
+                                                    </BreadcrumbItem>
+                                                    {index < breadcrumb.length - 1 && <BreadcrumbSeparator />}
+                                                </React.Fragment>
+                                            ))}
+                                        </BreadcrumbList>
+                                    </Breadcrumb>
+                                </div>
+                            </header>
+                        </>
+                    )
+                }
 
                 <div className="flex z-50 flex-1 flex-col gap-4 p-4 pt-0">
                     {children}
